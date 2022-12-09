29200a387e4c171c5ba7ef52b796b6a83189057e
John Ziamos <iziamos@gmail.com>
2022-12-09 22:53:22 +0000

    Day 8-2: More Rust, more tests

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -24,70 +24,82 @@ fn main() {
     let mut result = 0;
     for h in 0..height {
         for w in 0..width {
-            if is_invisible(&v, h, w) {
-                result += 1;
+            let s = score(&v, h, w);
+            if s > result {
+                result = s;
             }
+
+
         }
     }
 
-    println!("Result: {}", (height * width) - result);
+    println!("Result: {}", result);
 }
 
 
-fn is_invisible(input: &Vec<String>, h: u32, w: u32) -> bool {
+fn score (input: &Vec<String>, h: u32, w: u32) -> u32 {
     let l = input.get(h as usize).unwrap();
-    return is_invisible_horizontal(l, w) && is_invisible_vertical(input, h, w);
+    let horizontal = score_horizontal(&l, w);
+    let vertical = score_vertical(input, h, w);
+    return  horizontal * vertical;
 }
 
-fn is_invisible_horizontal(input: &String, wp: u32) -> bool {
+fn score_horizontal(input: &String, wp: u32) -> u32 {
     let width = input.len() as u32;
     let tree = height_at_line(&input, wp);
 
-        if (wp == width - 1) || wp == 0 {
-            return false;
-        }
+        let mut ret1 = 0;
 
-        let mut ret: bool = false;
-
-        for i in 0..wp {
-            if height_at_line(&input,  i) >= tree {
-                ret = true;
+        if wp > 0 {
+            for i in (0..wp).rev() {
+                ret1 += 1;
+                if height_at_line(&input,  i) >= tree {
+                    break;
+                }
             }
         }
 
+        let mut ret2 = 0;
 
-        for i in (wp + 1)..width {
-            if height_at_line(&input, i) >= tree {
-                return ret & true;
+        if wp < width - 1 {
+            for i in (wp + 1)..width {
+                ret2 += 1;
+                if height_at_line(&input, i) >= tree {
+                    break;
+                }
             }
         }
 
-        return false;
+        return ret1 * ret2;
 }
 
-fn is_invisible_vertical(input: &Vec<String>, hp: u32, wp: u32) -> bool {
+fn score_vertical(input: &Vec<String>, hp: u32, wp: u32) -> u32 {
     let height = input.len() as u32;
     let tree = height_at(&input, hp, wp);
 
-    if (hp == height - 1) || hp == 0 {
-        return false;
-    }
 
-    let mut ret: bool = false;
+    let mut ret1 = 0;
 
-    for i in (0..hp).rev() {
-        if height_at(&input,  i, wp) >= tree {
-            ret = true;
+    if hp > 0 {
+        for i in (0..hp).rev() {
+            ret1 += 1;
+            if height_at(&input,  i, wp) >= tree {
+                break;
+            }
         }
     }
 
-    for i in (hp + 1)..height {
-        if height_at(&input,  i, wp) >= tree {
-            return ret & true;
+    let mut ret2 = 0;
+    if hp < height - 1 {
+        for i in (hp + 1)..height {
+            ret2 += 1;
+            if height_at(&input,  i, wp) >= tree {
+                break;
+            }
         }
     }
 
-    return false;
+    return ret1 * ret2;
 }
 
 
@@ -106,71 +118,20 @@ fn height_at_line(input: &String, y: u32) -> u32 {
 mod tests {
     use super::*;
 
+
+
     #[test]
-    fn test_is_invisible_horizontal() {
-        assert_eq!(false, is_invisible_horizontal(&"12351".to_string(), 0));
-        assert_eq!(false, is_invisible_horizontal(&"12354".to_string(), 1));
-        assert_eq!(true, is_invisible_horizontal(&"32354".to_string(), 2));
-        assert_eq!(true, is_invisible_horizontal(&"32054".to_string(), 2));
-        assert_eq!(false, is_invisible_horizontal(&"12354".to_string(), 3));
-        assert_eq!(false, is_invisible_horizontal(&"123549".to_string(), 3));
-        assert_eq!(false, is_invisible_horizontal(&"12351".to_string(), 4));
-
-        assert_eq!(false, is_invisible_horizontal(&"11111".to_string(), 0));
-        assert_eq!(true, is_invisible_horizontal(&"11111".to_string(), 1));
-        assert_eq!(true, is_invisible_horizontal(&"11111".to_string(), 2));
-        assert_eq!(true, is_invisible_horizontal(&"11111".to_string(), 3));
-        assert_eq!(false, is_invisible_horizontal(&"11111".to_string(), 4));
-
-        assert_eq!(false, is_invisible_horizontal(&"10001".to_string(), 0));
-        assert_eq!(true, is_invisible_horizontal(&"10001".to_string(), 1));
-        assert_eq!(true, is_invisible_horizontal(&"10001".to_string(), 2));
-        assert_eq!(true, is_invisible_horizontal(&"10001".to_string(), 3));
-        assert_eq!(false, is_invisible_horizontal(&"10001".to_string(), 4));
-
-        assert_eq!(false, is_invisible_horizontal(&"30373".to_string(), 0));
-        assert_eq!(true, is_invisible_horizontal(&"30373".to_string(), 1));
-        assert_eq!(true, is_invisible_horizontal(&"30373".to_string(), 2));
-        assert_eq!(false, is_invisible_horizontal(&"30373".to_string(), 3));
-        assert_eq!(false, is_invisible_horizontal(&"30373".to_string(), 4));
+    fn test_score_horizontal() {
+        assert_eq!(0, score_horizontal(&"33200330".to_string(), 0));
+        assert_eq!(4, score_horizontal(&"33200330".to_string(), 5));
+        assert_eq!(0, score_horizontal(&"33200330".to_string(), 7));
+        assert_eq!(1, score_horizontal(&"33200330".to_string(), 3));
     }
 
     #[test]
-    fn test_height_at_line() {
-        assert_eq!(1, height_at_line(&"12351".to_string(), 0));
-        assert_eq!(2, height_at_line(&"12351".to_string(), 1));
-        assert_eq!(3, height_at_line(&"12351".to_string(), 2));
-        assert_eq!(6, height_at_line(&"12361".to_string(), 3));
-        assert_eq!(1, height_at_line(&"12351".to_string(), 4));
-    }
+    fn test_score_vertical() {
 
-    #[test]
-    fn test_height_at() {
-
-        let v: Vec<String> = vec![
-            "303".to_string(),
-            "252".to_string(),
-            "652".to_string()
-        ];
-
-
-        assert_eq!(3, height_at(&v, 0, 0));
-        assert_eq!(0, height_at(&v, 0, 1));
-        assert_eq!(3, height_at(&v, 0, 2));
-
-        assert_eq!(2, height_at(&v, 1, 0));
-        assert_eq!(5, height_at(&v, 1, 1));
-        assert_eq!(2, height_at(&v, 1, 2));
-
-        assert_eq!(6, height_at(&v, 2, 0));
-        assert_eq!(5, height_at(&v, 2, 1));
-        assert_eq!(2, height_at(&v, 2, 2));
-    }
-
-    #[test]
-    fn test_is_invisible_vertical() {
-
-        let v: Vec<String> = vec![
+    let v: Vec<String> = vec![
             "303".to_string(),
             "252".to_string(),
             "622".to_string(),
@@ -179,20 +140,14 @@ mod tests {
             "652".to_string()
         ];
 
+        assert_eq!(0, score_vertical(&v, 0, 1));
+        assert_eq!(2, score_vertical(&v, 1, 1));
+        assert_eq!(1, score_vertical(&v, 2, 1));
+        assert_eq!(6, score_vertical(&v, 3, 1));
+        assert_eq!(1, score_vertical(&v, 4, 1));
+        assert_eq!(0, score_vertical(&v, 5, 1));
 
-        assert_eq!(false, is_invisible_vertical(&v, 0, 1));
-        assert_eq!(false, is_invisible_vertical(&v, 1, 1));
-        assert_eq!(true,  is_invisible_vertical(&v, 2, 1));
-        assert_eq!(false, is_invisible_vertical(&v, 3, 1));
-        assert_eq!(true,  is_invisible_vertical(&v, 4, 1));
-        assert_eq!(false, is_invisible_vertical(&v, 5, 1));
-
-    }
-
-    #[test]
-    fn test_is_invisible() {
-
-        let v: Vec<String> = vec![
+        let v2: Vec<String> = vec![
             "30373".to_string(),
             "25512".to_string(),
             "65332".to_string(),
@@ -200,39 +155,23 @@ mod tests {
             "35390".to_string()
         ];
 
-        assert_eq!(false, is_invisible(&v, 0, 0)); //h //w
-        assert_eq!(false, is_invisible(&v, 1, 0));
-        assert_eq!(false, is_invisible(&v, 2, 0));
-        assert_eq!(false, is_invisible(&v, 3, 0));
-        assert_eq!(false, is_invisible(&v, 4, 0));
-
-
-        assert_eq!(false, is_invisible(&v, 0, 1));
-        assert_eq!(false, is_invisible(&v, 1, 1));
-        assert_eq!(false, is_invisible(&v, 2, 1));
-        assert_eq!(true, is_invisible(&v, 3, 1));
-        assert_eq!(false, is_invisible(&v, 4, 1));
-
-
-        assert_eq!(false, is_invisible(&v, 0, 2));
-        assert_eq!(false, is_invisible(&v, 1, 2));
-        assert_eq!(true, is_invisible(&v, 2, 2));
-        assert_eq!(false, is_invisible(&v, 3, 2));
-        assert_eq!(false, is_invisible(&v, 4, 2));
-
-
-        assert_eq!(false, is_invisible(&v, 0, 3));
-        assert_eq!(true, is_invisible(&v, 1, 3));
-        assert_eq!(false, is_invisible(&v, 2, 3));
-        assert_eq!(true, is_invisible(&v, 3, 3));
-        assert_eq!(false, is_invisible(&v, 4, 3));
-
-
-        assert_eq!(false, is_invisible(&v, 0, 4));
-        assert_eq!(false, is_invisible(&v, 1, 4));
-        assert_eq!(false, is_invisible(&v, 2, 4));
-        assert_eq!(false, is_invisible(&v, 3, 4));
-        assert_eq!(false, is_invisible(&v, 4, 4));
+        assert_eq!(2, score_vertical(&v2, 1, 2));
     }
+
+
+#[test]
+fn test_score() {
+    let v: Vec<String> = vec![
+        "30373".to_string(),
+        "25512".to_string(),
+        "65332".to_string(),
+        "33549".to_string(),
+        "35390".to_string()
+    ];
+
+    assert_eq!(4, score(&v, 1, 2));
+    assert_eq!(8, score(&v, 3, 2));
+}
+
 
 }
